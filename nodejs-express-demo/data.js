5effd5e3e440136fadfcb3d99bc2fc83f110ff62
Raj Dutta <rd2249619@gmail.com>
2021-04-12 18:47:22 +0600

    modified data.js file

--- nodejs-express-demo/data.js
+++ nodejs-express-demo/data.js
@@ -7,21 +7,69 @@ mongoose.connect("mongodb://localhost/store", { useUnifiedTopology: true, useNew
 
 // Schema Object
 const storeSchme = new mongoose.Schema({
-    name: String,
-    value: Number,
+    name: {
+        type: String,
+        minlength: 2,
+        maxlength: 255,
+        uppercase: false,
+        lowercase: true,
+        trim: true
+    },
+    value: {
+        type: Number,
+        // get: v => // Using some getter functionality,
+        // set: v => // Using some setter functionality
+    },
     isAvailable: Boolean
 });
 
 const studentSchema = new mongoose.Schema({
-    name: String,
+    name: { type: String, required: true },
     department: String,
-    isPassed: Boolean,
-    passingYear: Number
+    isPassed: { type: Boolean, required: true },
+    passingYear: {
+        type: Number,
+        required: function() {
+            return this.isPassed;
+        }
+    }
 });
 
 // Model
 const Store = mongoose.model('store', storeSchme);
 const Students = mongoose.model('students', studentSchema);
+
+const placeSchema = new mongoose.Schema({
+    name: { type: String, minlength: 2, maxlength: 255, require: function() { return true; } },
+    catogory: { type: String, enum: ['visited', 'favorite', 'dislike'], required: true},
+    reasons: {
+        type: Array,
+        validate: {
+            validator: function (v, callback) {
+                return new Promise(async(resolve, reject) => {
+                    const result = v && v.length > 0;
+                    await Store.find().then(obj => resolve(result)).catch(err => reject(err.message));
+                });
+            },
+            message: 'reason should have at last one tag'
+        }
+    }
+});
+
+
+const Places = mongoose.model('place', placeSchema);
+
+async function addPlaces(name, catogory, reasons) {
+    return new Promise(async (resolve, reject) => {
+        const place = new Places({
+            name,
+            catogory,
+            reasons
+        });
+
+        await place.save().then(obj => resolve(obj)).catch(err => reject(err));
+    });
+}
 
 async function updateData(id, name) {
     return new Promise(async (resolve, reject) => {
@@ -29,10 +77,10 @@ async function updateData(id, name) {
             $set: {
                 name
             }
-        }, { new: true }, function(e, r) {
-            console.log(e, r);
-            if ( r ) {
-                resolve(r);
+        }, { new: true }, function(e, s) {
+            console.log(e, s);
+            if ( s ) {
+                resolve(s);
             } else reject(e.message);
         }); 
     });
@@ -86,6 +134,23 @@ async function getIimitData() {
     const result = await Store.find().and([ { name: 'Lychee' }, { value: 20 } ]).sort({ name: 1 }).countDocuments().then(obj => console.log(obj)).catch(err => console.log(err.message));
 }
 
+async function fineOnes(id) {
+    await Store.findOneAndUpdate({ _id: id }, {
+        $set: {
+            value: 20
+        }
+    }, { new: true }, function (err, succ) {
+        console.log(err, succ);
+    });
+}
+
+
+async function getterThan() {
+    const result = await Store.find().or([{ value: 20 }, { value: 30 }]).then(obj => console.log(obj)).catch(err => console.log(err.message));
+}
+
+// getterThan();
+
 // Working with server
 const app = express();
 
@@ -118,6 +183,12 @@ app.post('/change', async (req, res) => {
     if ( id ) {
         await updateData(id, name).then(obj => res.send(obj)).catch(err => res.status(500).send(err.message));
     } else res.status(500).send("Data ain't properly send");
+});
+
+app.post('/places', async function (req, res) {
+    const { name, catogory, reasons } = req.body;
+
+    await addPlaces(name, catogory, reasons).then(obj => res.send(obj)).catch(err => res.status(500).send(err.message));
 });
 
 const port = process.env.PORT || 3000;
